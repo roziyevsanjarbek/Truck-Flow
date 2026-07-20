19b943118dcbe0e09d933c2ca3c338b009b9b74a
Sanjarbek Roziyev <roziyevsanjarbekk@gmail.com>
2026-07-20 16:31:57 +0500

    Fix UserFactory faker usage

--- app/Models/User.php
+++ app/Models/User.php
@@ -15,7 +15,7 @@ use Illuminate\Notifications\Notifiable;
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
-    use Notifiable;
+    use HasFactory, Notifiable;
 
     /**
      * Get the attributes that should be cast.
