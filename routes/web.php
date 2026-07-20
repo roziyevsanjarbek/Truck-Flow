e3781a799581628081ea36e269555571a0133693
Sanjarbek Roziyev <roziyevsanjarbekk@gmail.com>
2026-07-20 17:02:52 +0500

    fix bug

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,6 @@
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 
 Route::get('/drivers', [HomeController::class, 'driver'])->name('driver');
-Route::get('/cargo-requests', [HomeController::class, 'cargoRequest'])->name('cargo-request');
+Route::get('/', [HomeController::class, 'cargoRequest'])->name('cargo-request');
