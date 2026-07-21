bada72d3bd664bdb3dd7a8d5a0d3f865c34f783a
Sanjarbek Roziyev <roziyevsanjarbekk@gmail.com>
2026-07-21 13:55:29 +0500

    fix bug

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,9 @@
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/debug-file', function () {
+    return Storage::disk('public')->response('document/passport_6a5f1d912d0b7.jpg');
+});
 
 Route::get('/drivers', [HomeController::class, 'driver'])->name('driver');
 Route::get('/', [HomeController::class, 'cargoRequest'])->name('cargo-request');
