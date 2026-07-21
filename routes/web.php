24fbef8efc0292807cd9da1f5af717ca6e0f0bee
Sanjarbek Roziyev <roziyevsanjarbekk@gmail.com>
2026-07-21 13:57:14 +0500

    fix bug

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/debug-file', function () {
+    dd(123142);
     return Storage::disk('public')->response('document/passport_6a5f1d912d0b7.jpg');
 });
 
